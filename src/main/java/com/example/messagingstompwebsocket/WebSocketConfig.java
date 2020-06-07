23212503505cb67a42265e02f1e07e38e6c088bf
tblasCode <62831118+tblasCode@users.noreply.github.com>
2020-06-07 17:35:30 +0200

    Update WebSocketConfig.java

--- src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
+++ src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -5,6 +5,9 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -18,7 +21,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
+
+		RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+		registry.addEndpoint("/gs-guide-websocket")
+		.withSockJS();
+
+
+		registry.addEndpoint("/gs-guide-websocket").setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
+		.setAllowedOrigins("*");
 	}
 
 }
